bc198609cce68d7eeb083b7394dc0d147db3be02
tam-justin <144107518+tam-justin@users.noreply.github.com>
2025-07-10 22:44:51 -0700

    about page files created

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,22 @@ const aboutPage = function(){
     const background = document.createElement("div");
     background.id = "about-background";
 
+    const textSection = document.createElement("div");
+    textSection.id = "about-text";
+
+    const title = document.createElement("div");
+    title.id = "about-title";
+    title.innerHTML = "About";
+
+    const about = document.createElement("div");
+    about.id = "about-line";
+    about.innerHTML = "The Fancy Place is a high quality michelin star restaurant with a bistro-style menu. Here, you can find exquisite food with quality ingredients and wonderful service. We have been operating since 1990, and are proud to be one of the top restaurants in the world."
+
+    textSection.appendChild(title);
+    textSection.appendChild(about)
+    background.append(textSection);
+
+
     content.appendChild(background);
 }
 
